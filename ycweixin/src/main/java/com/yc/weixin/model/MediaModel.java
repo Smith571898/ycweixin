6a6c3b4d628a034773a9a28630052b092bcc33d5
Smith <2696358308@qq.com>
2017-08-11 11:21:16 +0800

    追加

---
+++ ycweixin/src/main/java/com/yc/weixin/model/MediaModel.java
@@ -0,0 +1,36 @@
+package com.yc.weixin.model;
+
+public class MediaModel {
+	//多媒体文件类型
+	public String type;
+	//视频id
+	public String thumb_media_id;
+	//媒体文件id
+	public String media_id;
+	//创建时间
+	public long created_at;
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getThumb_media_id() {
+		return thumb_media_id;
+	}
+	public void setThumb_media_id(String thumb_media_id) {
+		this.thumb_media_id = thumb_media_id;
+	}
+	public String getMedia_id() {
+		return media_id;
+	}
+	public void setMedia_id(String media_id) {
+		this.media_id = media_id;
+	}
+	public long getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(long created_at) {
+		this.created_at = created_at;
+	}
+}
---
+++ ycweixin/src/main/java/com/yc/weixin/web/controller/IndexController.java
@@ -0,0 +1,8 @@
+package com.yc.weixin.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	
+}
