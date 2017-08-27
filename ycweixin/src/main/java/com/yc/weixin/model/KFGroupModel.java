9a50bb7bb35fe6f8ec3bbd52a07f003408acef54
Smith <2696358308@qq.com>
2017-08-27 13:37:09 +0800

    客服接口以及Oauth2.0网页授权

---
+++ ycweixin/src/main/java/com/yc/weixin/model/KFGroupModel.java
@@ -0,0 +1,16 @@
+package com.yc.weixin.model;
+
+import java.util.List;
+
+public class KFGroupModel {
+
+	private List<KFModel> kf_list;
+
+	public List<KFModel> getKf_list() {
+		return kf_list;
+	}
+
+	public void setKf_list(List<KFModel> kf_list) {
+		this.kf_list = kf_list;
+	}
+}
